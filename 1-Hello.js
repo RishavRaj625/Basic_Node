62e48fcefd81c2433b2a27dfb3bd416793332f1b
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-10-20 23:14:25 +0530

    Add files via upload

---
+++ 1-Hello.js
@@ -0,0 +1,6 @@
+console.log("Hello Sir, This is my first code in Node Js... ")
+setTimeout(() => {
+    console.log("This is inside the settimeinterval... visible after 3 second")
+},3000)
+console.log("Namaste Sir")
+// console.log(Array,"array")
