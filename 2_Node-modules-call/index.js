bfaf61702a6c0af66c33ffc5f60be3444572dc8d
Rishav Raj <165421390+RishavRaj625@users.noreply.github.com>
2025-10-22 23:25:22 +0530

    Add files via upload

--- 2_Node-modules-call/index.js
+++ 2_Node-modules-call/index.js
@@ -1,22 +1,6 @@
-// module.exports = function() {};
-// require
+const loadsh = require('lodash');
 
-const  firstMod= require('./firstmod');
-console.log(firstMod.sum(2,3));
+const names = ['rishabh', 'rohn', 'Doe', 'Jane'];
+const captalizeName = loadsh.map(names, loadsh.capitalize);
+console.log(captalizeName);
 
-try{
-    let result = firstMod.divd(4,0);
-    console.log("This line will not be executed if an error occurs above");
-    console.log("Result of division:", result);
-}
-catch(err){
-    console.error("Error is caught in input\n", err.message)
-}
-
-// Module wrapper function
-
-(
-    function(exports, require, module, __filename, __dirname){
-        // Module code actually lives in here
-    }
-)
